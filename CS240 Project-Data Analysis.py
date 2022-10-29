9c04d61a8152cb35c7d824c118ca6a5cbd57d5c6
jiechengdeng <roydeng223@gmail.com>
2022-10-28 17:15:46 -0700

    3commit

--- CS240 Project-Data Analysis.py
+++ CS240 Project-Data Analysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[172]:
 
 
 import numpy as np
@@ -9,49 +9,42 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# In[5]:
+# In[173]:
 
 
 rawData = pd.read_csv('google_video.csv')
 
 
-# In[139]:
+# In[174]:
 
 
 print(rawData)
 print("(rows,columns):",rawData.shape)
 
 
-# In[140]:
+# In[175]:
 
 
+# Get np array by columns
 X = rawData.iloc[:,:86].values
 Y = rawData.iloc[:,86].values.reshape((1716,1))
-print(X)
-print('---------------------------------------------')
-print(Y)
+
+# Total length of flow in both direction
+X_total_length_flow = np.array(rawData.loc[:,['TotLen Fwd Pkts','TotLen Bwd Pkts','Category']])
+# Category and AppProtocol
+X_category_AppProtocol = np.array(rawData.loc[:,['Category','App_protocol']])
 
 
-# In[41]:
-
-
-# Category and Application protocol
-X_category_AppProtocol = X[:,84:86]
-X_category = X_category_AppProtocol[:,0]
-X_category = X_category.reshape(X_category.shape[0],1)
-
-X_appProtocol = X_category_AppProtocol[:,1]
-X_appProtocol = X_appProtocol.reshape(X_appProtocol.shape[0],1)
-
-print(X_category_AppProtocol)
-
-
-# In[37]:
+# In[167]:
 
 
 # plot category
 plt.style.use('seaborn')
+
+X_category = X_category_AppProtocol[:,0]
+X_category = X_category.reshape(X_category.shape[0],1)
 x_category_labels = np.unique(X_category)
+
 fig, ax = plt.subplots(figsize=(15,5))
 
 ax.hist(X_category,rwidth=0.5,bins=np.arange(-0.5,len(x_category_labels)),edgecolor='black')
@@ -59,7 +52,7 @@ ax.hist(X_category,rwidth=0.5,bins=np.arange(-0.5,len(x_category_labels)),edgeco
 plt.show()
 
 
-# In[133]:
+# In[168]:
 
 
 # plot what app protocols the network traffic use
@@ -77,7 +70,7 @@ ax.set_title("Network Traffic",fontsize=15)
 plt.show()
 
 
-# In[137]:
+# In[169]:
 
 
 # plot what app protocols the web traffic use
@@ -91,6 +84,45 @@ fig, ax = plt.subplots(figsize=(15,5))
 
 ax.hist(x_network_appProtocol,rwidth=0.3,bins=np.arange(-0.5,len(x_network_app_labels)),edgecolor='black')
 ax.set_title("Web Traffic",fontsize=15)
+plt.show()
+
+
+# In[197]:
+
+
+# plot total length of forward and backward flow in network traffic
+X_network_length_flow = X_total_length_flow[np.where(X_total_length_flow[:,2] == 'Network')]
+print(X_network_length_flow)
+flow = ['forward','backward']
+
+fig, ax = plt.subplots(figsize=(15,5))
+for i in range(2):
+    plt.plot(X_network_length_flow[:,i],label= flow[i])
+    
+plt.ylabel('length in bytes')
+plt.xlabel('number of flows')
+
+legend = plt.legend(loc='upper center', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+plt.show()
+
+
+# In[198]:
+
+
+# plot total length of forward and backward flow in web traffic
+X_web_length_flow = X_total_length_flow[np.where(X_total_length_flow[:,2] == 'Web')]
+fig, ax = plt.subplots(figsize=(15,5))
+for i in range(2):
+    plt.plot(X_web_length_flow[:,i],label= flow[i])
+    
+plt.ylabel('length in bytes')
+plt.xlabel('number of flows')
+
+legend = plt.legend(loc='upper center', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
 plt.show()
 
 
